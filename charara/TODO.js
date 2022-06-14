8f69fa3c448d11d5bf825ffd9a4bb2fd0a6f3ad1
Noise-Wall <107452784+Noise-Wall@users.noreply.github.com>
2022-06-13 21:56:13 -0600

    Add files via upload

---
+++ charara/TODO.js
@@ -0,0 +1,26 @@
+/*  TODO
+
+Responsive design: 
+    -make top rank chara boxes smaller for <480 
+    -set justify items of <768 to space-between
+    -page title overflows, fix it
+
+HTML2Canvas fixes:
+    -textarea does not work. remove, change back to input
+        -then adjust height between chara items.
+    -instead of opening new tab, show on a mini popup, with a button to download (possibly share buttons in the future as well)
+        -download button gives choice to download to specific folder
+
+-drag image files to imgBar to upload them
+
+-right click to return image to imgBar
+-switch name input when dragging and dropping an image that is already on a chara container
+
+TITLE ABILITY
+    -give a space on top of the rows where the Podium logo and trademark (?) that will show on final image
+    -Underneath, a label with the title of the ranker
+
+PROMISES
+    -switch stuff to promises.then
+
+*/
